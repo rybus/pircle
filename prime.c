86a7e6ed8088b1c1cfd23cfc80867284b7899497
Remy Betus <remy.betus@akeneo.com>
2019-04-07 21:41:41 -0400

    first drawings

---
+++ prime.c
@@ -0,0 +1,150 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <SDL2/SDL.h>
+#include <stdint.h>
+
+void pause();
+void draw_circle(SDL_Renderer* renderer, SDL_Point center, int radius, SDL_Color color);
+void draw_point_on_circle(SDL_Renderer* renderer, SDL_Point center, double radian, int radius, SDL_Color color);
+int check_prime(int a);
+void draw_n_primes(SDL_Renderer* renderer,  SDL_Point center, int radius, int number_of_primes);
+
+int main(int argc, char *argv[])
+{
+    SDL_Window *window;
+    SDL_Renderer* renderer;
+    
+
+    SDL_Point window_position = {         
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED
+    };
+    SDL_Point center = {         
+        405,
+        405
+    };
+
+    SDL_Point window_size = {810, 810};   
+    SDL_Color blue = {0, 120, 199};
+   
+
+    SDL_Init(SDL_INIT_VIDEO);             
+
+
+    window = SDL_CreateWindow( 
+        "Primes on a circle",               
+        window_position.x,               
+        window_position.y,                
+        window_size.x,                   
+        window_size.y,                   
+        SDL_WINDOW_OPENGL                 
+    );
+
+    // Check that the window was successfully made
+    if (window == NULL) {
+        SDL_Log("Could not create window: %s", SDL_GetError());
+        return 1;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    for (int y = 2; y <= 600; y = y+1) {       
+        draw_n_primes(renderer, center, y, y+1000);
+    }
+
+    SDL_RenderPresent(renderer);
+
+    pause();
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window); 
+
+    // Clean up
+    SDL_Quit();
+    return 0; 
+}
+
+void draw_n_primes(SDL_Renderer* renderer,  SDL_Point center, int radius, int number_of_primes)
+{
+    double le_point = 0.0;
+    SDL_Color red = {255, 66, 55};
+
+    for (int i = 0; i <= number_of_primes; i++) {
+        if (check_prime(i)) {
+            le_point = (i*2*M_PI)/number_of_primes;
+            SDL_Color red = {radius*255/600, 0, radius*55/600};
+            draw_point_on_circle(renderer, center, le_point, radius, red);
+        }
+    }
+}
+
+int check_prime(int a)
+{
+   int c;
+   
+   for ( c = 2 ; c <= a - 1 ; c++ )
+   {
+      if ( a%c == 0 )
+     return 0;
+   }
+   if ( c == a )
+      return 1;
+}
+
+void draw_circle(SDL_Renderer* renderer, SDL_Point center, int radius, SDL_Color color)
+{
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+	int x, y, m;
+	x = 0;
+	y = radius;             
+	m = 5 - 4*radius;
+	while (x <= y)        
+    {
+        SDL_RenderDrawPoint(renderer, x  + center.x,  y + center.y);
+		SDL_RenderDrawPoint(renderer, x  + center.x,  y + center.y);
+		SDL_RenderDrawPoint(renderer, y  + center.x,  x + center.y);
+		SDL_RenderDrawPoint(renderer, -x + center.x,  y + center.y);
+		SDL_RenderDrawPoint(renderer, -y + center.x,  x + center.y);
+		SDL_RenderDrawPoint(renderer, x  + center.x, -y + center.y);
+		SDL_RenderDrawPoint(renderer, y  + center.x, -x + center.y);
+		SDL_RenderDrawPoint(renderer, -x + center.x, -y + center.y);
+		SDL_RenderDrawPoint(renderer, -y + center.x, -x + center.y);
+
+		if (m > 0) {
+			y = y - 1;
+			m = m - 8*y;
+		}
+
+		x = x + 1;
+		m = m + 8*x + 4;
+	}
+}
+
+void draw_point_on_circle(SDL_Renderer* renderer, SDL_Point center, double radian, int radius, SDL_Color color)
+{
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+	double x, y;
+	x = cos(radian) * radius;
+	y = sin(radian) * radius; 
+	
+    SDL_RenderDrawPoint(renderer, x  + center.x,  y + center.y);
+    SDL_RenderDrawPoint(renderer, y  + center.x,  x + center.y);
+}
+
+
+void pause()
+{
+    int continuer = 1;
+    SDL_Event event;
+ 
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
+}
